d834a04024c19c224fa929f3b5fb0dc92ab0a02d
Phat Tran <trankyphat@gmail.com>
2018-02-19 16:38:55 +0700

    Example: 2 - Observable, Subscibtion

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
-var count = 0;
-var rate = 1000;
-var lastClick = Date.now() - rate;
-var normalBtn = document.querySelector('#normal');
-normalBtn.addEventListener('click', (event) => {
-  if (Date.now() - lastClick >= rate) {
-    count += event.clientX;
-    console.log(`Normal Clicked ${count} times`);
-    lastClick = Date.now();
-  }
+var funcBtn = document.querySelector('#func');
+var subscribeBtn = document.querySelector('#subscribe-btn');
+var unsubscribeBtn = document.querySelector('#unsubscribe-btn');
+
+function sampleFunction() {
+  console.log('Hello World');
+  return 'Pure Function';
+  return 'more...'
+}
+
+// Create Observable
+const observableFunc = Rx.Observable.create(function (observer) {
+  // 1.
+  console.log('Hello');
+  observer.next(42);
+
+  setInterval(() => {
+    observer.next('Kaka');
+  }, 2000);
+
+  // 2.
+  // setTimeout(() => {
+  //   observer.error('Error...');
+  // }, 4000)
+
+  // setTimeout(() => {
+  //   observer.complete();
+  // }, 6000)
 });
 
-var button = document.querySelector('#observable');
-Rx.Observable.fromEvent(button, 'click')
-  .throttleTime(1000)
-  .map(event => event.clientX)
-  .scan((count, clientX) => count + clientX, 0)
-  .subscribe(count => console.log(`Observable Clicked ${count} times`));
+// Call sample function while click
+funcBtn.addEventListener('click', (event) => {
+  const result = sampleFunction();
+  console.log(result);
+});
+
+var subscription;
+// Subscribe
+subscribeBtn.addEventListener('click', (event) => {
+  console.log('Subscribe....');
+  // 1.
+  // subscription = observableFunc.subscribe((data) => {
+  //   console.log(data);
+  // });
+
+  // 2.
+  var observer = {
+    next: x => console.log('Observer got a next value: ' + x),
+    error: err => console.error('Observer got an error: ' + err),
+    complete: () => console.log('Observer got a complete notification'),
+  };
+  subscription = observableFunc.subscribe(observer);
+});
+
+unsubscribeBtn.addEventListener('click', (event) => {
+  console.log('Unsubscribe....');
+  subscription.unsubscribe();
+});
+
