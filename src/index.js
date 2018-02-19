e3ad4b32af9b64f7de0341ece2d41633891c0853
Phat Tran <trankyphat@gmail.com>
2018-02-19 13:37:21 +0700

    Example: 1.2 - Flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 var count = 0;
+var rate = 1000;
+var lastClick = Date.now() - rate;
 var normalBtn = document.querySelector('#normal');
-normalBtn.addEventListener('click', () => console.log(`Normal Clicked ${++count} times`));
+normalBtn.addEventListener('click', () => {
+  if (Date.now() - lastClick >= rate) {
+    console.log(`Normal Clicked ${++count} times`);
+    lastClick = Date.now();
+  }
+});
 
 var button = document.querySelector('#observable');
 Rx.Observable.fromEvent(button, 'click')
+  .throttleTime(1000)
   .scan(count => count + 1, 0)
   .subscribe(count => console.log(`Observable Clicked ${count} times`));
