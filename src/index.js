b58ccfb0962dd7aa334fd97064db6c89692b2d4b
Phat Tran <trankyphat@gmail.com>
2018-02-19 13:40:50 +0700

    Examples: 1.3 - value

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ var count = 0;
 var rate = 1000;
 var lastClick = Date.now() - rate;
 var normalBtn = document.querySelector('#normal');
-normalBtn.addEventListener('click', () => {
+normalBtn.addEventListener('click', (event) => {
   if (Date.now() - lastClick >= rate) {
-    console.log(`Normal Clicked ${++count} times`);
+    count += event.clientX;
+    console.log(`Normal Clicked ${count} times`);
     lastClick = Date.now();
   }
 });
@@ -12,5 +13,6 @@ normalBtn.addEventListener('click', () => {
 var button = document.querySelector('#observable');
 Rx.Observable.fromEvent(button, 'click')
   .throttleTime(1000)
-  .scan(count => count + 1, 0)
+  .map(event => event.clientX)
+  .scan((count, clientX) => count + clientX, 0)
   .subscribe(count => console.log(`Observable Clicked ${count} times`));
