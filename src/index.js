74de366fe4ac01117b1a98369160f5cc25f190ea
Phat Tran <trankyphat@gmail.com>
2018-02-19 13:32:06 +0700

    Examples 1.1: Purity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('asdasdasd');
+var count = 0;
+var normalBtn = document.querySelector('#normal');
+normalBtn.addEventListener('click', () => console.log(`Normal Clicked ${++count} times`));
+
+var button = document.querySelector('#observable');
+Rx.Observable.fromEvent(button, 'click')
+  .scan(count => count + 1, 0)
+  .subscribe(count => console.log(`Observable Clicked ${count} times`));
