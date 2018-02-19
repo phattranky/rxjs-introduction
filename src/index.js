89e25ea513d8346799c50d679782998e92c13763
Phat Tran <trankyphat@gmail.com>
2018-02-19 18:00:45 +0700

    Example 3 - Subject - Multicasted Observable

--- src/index.js
+++ src/index.js
@@ -1,59 +1,14 @@
-var funcBtn = document.querySelector('#func');
-var subscribeBtn = document.querySelector('#subscribe-btn');
-var unsubscribeBtn = document.querySelector('#unsubscribe-btn');
+var source = Rx.Observable.from([1, 2, 3]);
+var subject = new Rx.Subject();
+var multicasted = source.multicast(subject);
 
-function sampleFunction() {
-  console.log('Hello World');
-  return 'Pure Function';
-  return 'more...'
-}
-
-// Create Observable
-const observableFunc = Rx.Observable.create(function (observer) {
-  // 1.
-  console.log('Hello');
-  observer.next(42);
-
-  setInterval(() => {
-    observer.next('Kaka');
-  }, 2000);
-
-  // 2.
-  // setTimeout(() => {
-  //   observer.error('Error...');
-  // }, 4000)
-
-  // setTimeout(() => {
-  //   observer.complete();
-  // }, 6000)
+// These are, under the hood, `subject.subscribe({...})`:
+multicasted.subscribe({
+  next: (v) => console.log('observerA: ' + v)
+});
+multicasted.subscribe({
+  next: (v) => console.log('observerB: ' + v)
 });
 
-// Call sample function while click
-funcBtn.addEventListener('click', (event) => {
-  const result = sampleFunction();
-  console.log(result);
-});
-
-var subscription;
-// Subscribe
-subscribeBtn.addEventListener('click', (event) => {
-  console.log('Subscribe....');
-  // 1.
-  // subscription = observableFunc.subscribe((data) => {
-  //   console.log(data);
-  // });
-
-  // 2.
-  var observer = {
-    next: x => console.log('Observer got a next value: ' + x),
-    error: err => console.error('Observer got an error: ' + err),
-    complete: () => console.log('Observer got a complete notification'),
-  };
-  subscription = observableFunc.subscribe(observer);
-});
-
-unsubscribeBtn.addEventListener('click', (event) => {
-  console.log('Unsubscribe....');
-  subscription.unsubscribe();
-});
-
+// This is, under the hood, `source.subscribe(subject)`:
+multicasted.connect();
